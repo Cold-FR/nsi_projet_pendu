ef5437828ec367ee3d90f6d63c0abbd88ac6545d
Cold-FR <51854565+Cold-FR@users.noreply.github.com>
2022-05-03 09:45:31 +0200

    Update server

--- server.py
+++ server.py
@@ -1,14 +1,27 @@
+from crypt import methods
 from flask import Flask, request, render_template, jsonify
 from game import game
 
 app = Flask(__name__)
 
+parties = [
+    {'id': 1, 'word': 'TUER', 'progression': 'T_E_'}
+]
 
 @app.route('/', methods=['GET'])
 @app.route('/index.html', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
-    
+
+@app.route('/actions/start_game', methods=['GET'])
+def startGame():
+    global parties
+    partyId = request.args.get('id')
+
+@app.route('/actions/create_game', methods=['POST'])
+def createGame():
+    global parties
+    print('a')
 
 app.run(host='0.0.0.0', port='5001', debug=True)
